2efe81d8c95858cfbc1531b3201ac9130081ac6a
butchtabios <butchtabios@live.com>
2014-10-16 10:22:25 +0800

    implemented new slow decay

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#pragma config OSC=INTIO1
+#pragma config OSC=HSPLL
 #pragma config FSCM=OFF
 #pragma config IESO=OFF
 
@@ -39,35 +39,26 @@ typedef union {
     unsigned char BYTE;
 } Byte_t;
 
-#define SLOW_DECAY  0   //0=fast decay, 1=slow decay
-#define PHASE_SIZE  65  //for 60Hz @ 15.625Khz PWM freq.
+#define SLOW_DECAY  1   //0=fast decay, 1=slow decay
+#define PHASE_SIZE  81  //for 60Hz @ 19.531Khz PWM freq.
 #define PWM_PERIOD  128 // @ 15.625Khz PWM
 
 volatile unsigned char phase1;
 volatile Byte_t phase2;
 
 const unsigned char PWM_VAL[PHASE_SIZE] =
-#if 1
-{    2,    4,    8,   11,   14,   17,   20,   23,
-    26,   29,   31,   33,   37,   39,   43,   46,
-    48,   51,   54,   56,   59,   62,   64,   67,
-    69,   72,   74,   77,   79,   81,   83,   86,
-    88,   90,   92,   94,   96,   98,  100,  102,
-   103,  105,  107,  108,  110,  111,  113,  114,
-   116,  117,  118,  119,  120,  121,  122,  123,
-   124,  124,  125,  125,  126,  126,  126,  127,
+{    1,  4,  6,  9, 11, 14, 16, 18,
+    21, 23, 26, 28, 30, 33, 35, 37,
+    40, 42, 44, 46, 49, 51, 53, 55,
+    57, 61, 61, 64, 66, 68, 70, 72,
+    74, 76, 78, 80, 82, 83, 85, 87,
+    89, 91, 93, 94, 96, 97, 99,101,
+   102,103,105,106,108,109,110,111,
+   113,114,115,116,117,118,119,120,
+   120,121,122,122,123,124,124,125,
+   125,126,126,126,126,127,127,127,
    127};
-#else
-{    0,    3,    6,    9,   12,   15,   18,   21,
-    24,   27,   30,   33,   36,   39,   42,   45,
-    48,   51,   54,   56,   59,   62,   64,   67,
-    70,   72,   75,   77,   80,   82,   84,   86,
-    89,   91,   93,   95,   97,   99,  101,  103,
-   105,  106,  108,  109,  111,  112,  114,  115,
-   116,  118,  119,  120,  121,  122,  123,  123,
-   124,  125,  125,  126,  126,  126,  127,  127,
-   127};
-#endif
+
 /*---------------------------------------------------------*/
 /* High-priority service */
 
@@ -81,12 +72,18 @@ void interrupt high_isr(void)
     LATBbits.LATB0 = 0; //enable IR2110
 
 #if SLOW_DECAY
-    CCPR1L = PWM_VAL[phase1];
+    if (CCP1CONbits.P1M1==0)
+        CCP1CONbits.P1M1 = 1;
+    else
+        CCP1CONbits.P1M1 = 0;
 #endif
     if (phase2.BIT1==0)
     {
 #if SLOW_DECAY
-        CCP1CONbits.P1M1 = 0;
+        if (CCP1CONbits.P1M1==0)
+            CCPR1L = PWM_PERIOD + PWM_VAL[phase1];
+        else
+            CCPR1L = PWM_PERIOD - PWM_VAL[phase1];
 #else
         CCPR1L = (PWM_PERIOD + PWM_VAL[phase1])>>1;
 #endif
@@ -94,12 +91,19 @@ void interrupt high_isr(void)
     else
     {
 #if SLOW_DECAY
-        CCP1CONbits.P1M1 = 1;
+        if (CCP1CONbits.P1M1==1)
+            CCPR1L = PWM_PERIOD + PWM_VAL[phase1];
+        else
+            CCPR1L = PWM_PERIOD - PWM_VAL[phase1];
 #else
         CCPR1L = (PWM_PERIOD - PWM_VAL[phase1])>>1;
 #endif
     }
 
+#if SLOW_DECAY
+    if (CCP1CONbits.P1M1==0)
+    {
+#endif
     if (phase2.BIT0==0)
     {
         phase1++;
@@ -118,6 +122,9 @@ void interrupt high_isr(void)
             phase1 = 0;
         }
     }
+#if SLOW_DECAY
+    }
+#endif
 }
 
 /* Low-priority interrupt routine */
@@ -150,12 +157,16 @@ int main(int argc, char** argv)
     TRISB = 0b00110010; //P1A, P1B, P1C, P1D and PORTB0 are outputs
 
     /* Initialize peripherals */
+#if SLOW_DECAY
+    PR2 = (PWM_PERIOD*2)-1; //9-bit resolution 15.625KHz @ 8Mhz
+#else
     PR2 = PWM_PERIOD-1; //9-bit resolution 15.625KHz @ 8Mhz
+#endif
     T2CON = 0;
     TMR2ON = 1;
     CCP1CONbits.CCP1M = 0b1100; // P1A, P1C active-high; P1B, P1D active-high
     CCP1CONbits.DC1B = 0b00;
-#if SOFT_DECAY
+#if SLOW_DECAY
     CCP1CONbits.P1M = 0b01; //Quad PWM
 #else
     CCP1CONbits.P1M = 0b10; //Half-bridge
